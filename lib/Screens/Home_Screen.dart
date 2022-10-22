2c3e70b163cf68685848e3a654963d3bbc624df3
Madhav2008 <aroramadhav65@gmail.com>
2022-10-22 23:11:33 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -8,7 +8,7 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> {final
   @override
   Widget build(BuildContext context) {
     return Stack(
