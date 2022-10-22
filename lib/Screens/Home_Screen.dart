bb3827a626466fb6a648d79f4e4d9af62043fd00
Madhav2008 <aroramadhav65@gmail.com>
2022-10-22 23:13:15 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -10,6 +10,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final _controller = ConfettiController();
+  
   @override
   Widget build(BuildContext context) {
     return Stack(
