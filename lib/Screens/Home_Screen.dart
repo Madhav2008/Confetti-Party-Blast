c12f8239a823c95630014138f1a0387373fc7d3f
Madhav2008 <aroramadhav65@gmail.com>
2022-10-22 23:07:38 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -12,7 +12,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Scaffold(),
+        Scaffold(appBar: ,),
       ],
     );
   }
