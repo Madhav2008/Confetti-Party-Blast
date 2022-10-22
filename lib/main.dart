4ff64d977d12a38abc6b5d88a77174b129dac38d
Madhav2008 <aroramadhav65@gmail.com>
2022-10-22 23:09:25 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Confetti',
+      title: 'Confetti',debugShowCheckedModeBanner: ,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
