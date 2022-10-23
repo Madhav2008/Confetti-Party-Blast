b889270f41134f386988d82016872b271f78a92a
Madhav2008 <aroramadhav65@gmail.com>
2022-10-23 15:41:21 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -47,7 +47,7 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         ConfettiWidget(
           confettiController: _controller,
-          blastDirection: pi,
+          blastDirection: pi/2,
         ),
       ],
     );
