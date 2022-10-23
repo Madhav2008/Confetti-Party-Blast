01dbb4c3e9b26a9e1a86e45a1b700a077ac69e97
Madhav2008 <aroramadhav65@gmail.com>
2022-10-23 15:35:11 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -27,7 +27,7 @@ class _HomeScreenState extends State<HomeScreen> {
           body: Center(
             child: MaterialButton(
               onPressed: () {
-                if (isPlaying) {}else
+                if (isPlaying) {}else{}
                 isPlaying = !isPlaying;
               },
               child: Text('Party Time'),
