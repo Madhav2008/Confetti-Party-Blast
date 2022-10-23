aa7807004ebe66516cb3244a186268ce1169b3ee
Madhav2008 <aroramadhav65@gmail.com>
2022-10-23 15:48:19 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -28,7 +28,7 @@ class _HomeScreenState extends State<HomeScreen> {
           appBar: AppBar(
             title: Text(
               'CONFETTI',
-              style: TextStyle(letterSpacing: ),
+              style: TextStyle(letterSpacing: 1),
             ),
             centerTitle: true,
           ),
