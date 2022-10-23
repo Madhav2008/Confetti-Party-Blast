52b976833c421df57ec9a320df627a90c5d959c1
Madhav2008 <aroramadhav65@gmail.com>
2022-10-23 15:57:25 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: HomeScreen(),
+      home: const HomeScreen(),
     );
   }
 }
