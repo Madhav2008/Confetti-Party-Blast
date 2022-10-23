38bb23353931221a8445d818ba6487e9e2750e4b
Madhav2008 <aroramadhav65@gmail.com>
2022-10-23 15:38:54 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -25,7 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
       children: [
         Scaffold(
           appBar: AppBar(
-            title: Text('CONFETTI'),centerTitle: ,
+            title: Text('CONFETTI'),centerTitle: true,
           ),
           body: Center(
             child: MaterialButton(
