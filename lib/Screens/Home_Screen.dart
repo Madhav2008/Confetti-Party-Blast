203d495b1078187112efc02b4b0ac8e24b60d392
Madhav2008 <aroramadhav65@gmail.com>
2022-10-23 15:38:34 +0530

    Update

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -25,7 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
       children: [
         Scaffold(
           appBar: AppBar(
-            title: Text(),
+            title: Text(''),
           ),
           body: Center(
             child: MaterialButton(
